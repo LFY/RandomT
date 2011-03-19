a9dafac3478a58e3d7171e7f7b241ed6a9e77aa6
lyang <lyang@dnab434de2.stanford.edu>
2011-03-19 12:10:22 -0700

    adding basic examples

--- examples/watersprinkler.py
+++ examples/watersprinkler.py
@@ -3,7 +3,7 @@ from RandomT import *
 Flip = lambda p: RndVar(Dist({1 : p, 0 : 1 - p}))
 
 @rbind
-def Grass(x, y):
+def GrassWet(x, y):
 	if x == 1 and y == 1:
 		return Flip(0.99)
 	elif x == 0 and y == 1:
@@ -30,9 +30,18 @@ def Rain(x):
 C = Flip(0.5)
 S = Sprinkler(C)
 R = Rain(C)
-W = Grass(S, R)
+W = GrassWet(S, R)
 
 print Pr(W, {}, VarElim) 
+
+print Pr(S, {W : 1, R: 0}, VarElim)
 print Pr(W, {}, RejectionSampler) 
 
 print debug_output(W, locals())
+
+print 'another way to specify'
+
+Cloudy = Flip(0.5)
+Wet = GrassWet(Sprinkler(Cloudy), Rain(Cloudy))
+
+print Pr(Wet, {}, VarElim)
